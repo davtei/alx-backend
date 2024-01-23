43a8995dc637315f1de4cd4ac75f4890794d459f
David Tei <daveteidt@gmail.com>
2024-01-23 23:31:59 +0000

    Add FIFO caching implementation

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" FIFO caching """
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFO cache system """
+
+    def __init__(self):
+        """ Initialize class instance """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Store key/value pair to cache_data """
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print(f"DISCARD: {self.order[0]}")
+                del self.cache_data[self.order[0]]
+                del self.order[0]
+            self.order.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Return value linked to key """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
